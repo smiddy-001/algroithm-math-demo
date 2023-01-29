68c6aed3cbdea8718c3142a320bacdeaa247a7ac
riley smith <rileys1000@gmail.com>
2023-01-29 15:41:45 +1300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+// import Color from './color.json';
+
+let colors = require('./color.json');
+console.log(colors[0].hex);
+
+function Button(props) {
+  return (
+    <button style={{borderRadius:0, padding:8, marginLeft: 2, marginRight: 2}}id={props.text}>{props.text}</button>
+  )
+}
+
+function Dropdown() {
+  return (
+    <div id="dropdown"></div>
+  )
+}
+
+function InputField() {
+  return (
+    <div id="me">whopsies</div>
+  )
+}
+
+function TabContainer() {
+  return (
+    <div id="me">whopsies</div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="grid-container">
+        <div id="tabs-container">t1</div>
+        <div id="tabs-header-container">t2</div>
+        <div id="header-container">
+          <div id="left-align">
+        left
+          </div>
+          <div id="right-align">
+            <Button text='reset'/>
+            <Button text='import'/>
+            <Button text='export'/>
+            <Button text='save'/>
+          </div>
+        </div>
+        <div id="body-container">body</div>
+      </div>
     </div>
   );
 }
